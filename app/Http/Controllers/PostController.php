d3c5d0cefd80c76dff05280f8b712353c4cc3b10
tatsuya730 <t.taniguchi730@gmail>
2023-11-04 17:19:58 +0900

    リファクタリング終了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,7 +35,7 @@ class PostController extends Controller
         $post->user_id = $request->user()->id;
         
         $file = $request->file('image');
-        $post->image = date('YmdHis') . '_' .
+        $post->image = self::createFileName($file);
             $file->getClientOriginalName();
 
         // トランザクション開始
@@ -78,7 +78,9 @@ class PostController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('posts.edit', compact('post'));
     }
 
     /**
@@ -86,7 +88,52 @@ class PostController extends Controller
      */
     public function update(UpdatePostRequest $request, string $id)
     {
-        //
+        $post = Post::find($id);
+
+        if ($request->user()->cannot('update', $post)) {
+            return redirect()->route('posts.show', $post)
+                ->withErrors('自分の記事以外は更新できません');
+        }
+
+        $file = $request->file('image');
+        if ($file) {
+            $delete_file_path = 'images/posts/' . $post->image;
+                        $post->image = self::createFileName($file);
+        }
+        $post->fill($request->all());
+
+        // トランザクション開始
+        DB::beginTransaction();
+        try {
+            // 更新
+            $post->save();
+
+            if ($file) {
+                // 画像アップロード
+                if (!Storage::putFileAs('images/posts', $file, $post->image)) {
+                    // 例外を投げてロールバックさせる
+                    throw new \Exception('画像ファイルの保存に失敗しました。');
+                }
+
+                // 画像削除
+                if (!Storage::delete($delete_file_path)) {
+                    //アップロードした画像を削除する
+                    Storage::delete('images/posts/' . $post->image);
+                    //例外を投げてロールバックさせる
+                    throw new \Exception('画像ファイルの削除に失敗しました。');
+                }
+            }
+
+            // トランザクション終了(成功)
+            DB::commit();
+        } catch (\Exception $e) {
+            // トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('posts.show', $post)
+            ->with('notice', '記事を更新しました');
     }
 
     /**
@@ -95,5 +142,10 @@ class PostController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    private static function createFileName($file)
+    {
+        return date('YmdHis') . '_' . $file->getClientOriginalName();
     }
 }
